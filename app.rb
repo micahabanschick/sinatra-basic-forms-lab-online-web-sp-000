eee72b45c24b301738bfa4196ad81e9368d7e2e5
Micah Banschick <micha.ban@gmail.com>
2020-08-07 16:45:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :index 
+  end 
+  
+  get '/new' do 
+    erb :create_puppy 
+  end 
+  
+  post '/puppy' do 
+    # binding.pry 
+    @name = params['name']
+    @age = params['age']
+    @breed = params['breed']
+    erb :display_puppy
+  end 
 
 end
